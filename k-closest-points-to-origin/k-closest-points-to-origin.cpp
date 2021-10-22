aaf51658354e9f75e0ae39cc6c4b21df54b268ed
Soumya_EE_18 <50716398+basaksoumya@users.noreply.github.com>
2021-10-22 13:08:14 +0530

    Time: 241 ms (51.79%), Space: 49.2 MB (94.87%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,31 +1,31 @@
 class Solution {
-public:
-    typedef pair<double,vector<int>> pii;
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        
-        priority_queue<pii> pq;
-        vector<vector<int>> ans;
-        int size = 0, n = points.size();
-        
-        if(k == n) return points;
-        
-        for(int i = 0 ; i < n ; i++) {
-            vector<int> curr = points[i];
-            double dist = sqrt(curr[0]*curr[0] + curr[1] * curr[1]);
-            if(pq.size() < k) {
-                pq.push({dist,curr});
-            }
-            else if(pq.top().first > dist) {
-                pq.pop();
-                pq.push({dist,curr});
+    static inline int getsum(const vector<int> &v){ return v[0]*v[0]+v[1]*v[1]; }
+    static inline bool comp(const vector<int> &l, const vector<int> &r) { return getsum(l)<=getsum(r); }
+     
+    //standard Quicksort(Lomuto) partition
+    int partition(vector<vector<int>>& arr, int left, int right){
+        vector<int> pivot = arr[right];
+        int partitionIndex = left;
+        for(int i = left; i<right; i++){
+            if (comp(arr[i], pivot)) {//custom comparator for point (x, y)
+                swap(arr[i],arr[partitionIndex]);
+                partitionIndex++;
             }
         }
-        
-        while(pq.size()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        
-        return ans;
+        swap(arr[partitionIndex], arr[right]);
+        return partitionIndex;
     }
+    void quickselect(vector<vector<int>>&v, int l, int r, int K){
+        while(l<=r){
+             int mid = partition(v, l, r);
+             if (mid==K) return;
+             if (mid<K) l = mid+1;
+             else r = mid - 1;
+        }
+    }
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        quickselect(points, 0, points.size()-1, K);
+        return vector<vector<int>>(points.begin(), points.begin() + K);
+    }    
 };
